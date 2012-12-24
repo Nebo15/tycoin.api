655ee654facc80612484d4ac68fc03dc76c5a323
Andrew Dryga <andrew@dryga.com>
2012-12-24 18:25:58 +0200

    Eng

--- src/controller/PartnersController.class.php
+++ src/controller/PartnersController.class.php
@@ -8,13 +8,13 @@ class PartnersController extends BaseJsonController
     $offers = [];
     $offer1 = new stdClass();
     $offer1->id = 1;
-    $offer1->description = 'Бесплатный кофе за каждое хорошее дело!';
+    $offer1->description = 'Free coffee for all nice people!';
     $offer1->image = 'http://upload.wikimedia.org/wikipedia/commons/thumb/4/45/A_small_cup_of_coffee.JPG/800px-A_small_cup_of_coffee.JPG';
     $offer1->coins = 1;
     $offer1->coin_type = 1;
     $offer1->shop = new stdClass();
-    $offer1->shop->title = 'Кофейня "Венеция"';
-    $offer1->shop->location = "ул. Ленина, 4а";
+    $offer1->shop->title = 'Coffee House "Kiev"';
+    $offer1->shop->location = "Lenina street, 4a";
     $offers[] = $offer1;
     return $this->_answerOk($offers);
   }
@@ -23,13 +23,13 @@ class PartnersController extends BaseJsonController
   {
     $offer = new stdClass();
     $offer->id = 1;
-    $offer->description = 'Бесплатный кофе за каждое хорошее дело!';
+    $offer->description = 'Free coffee for all nice people!';
     $offer->image = 'http://upload.wikimedia.org/wikipedia/commons/thumb/4/45/A_small_cup_of_coffee.JPG/800px-A_small_cup_of_coffee.JPG';
     $offer->coins = 1;
     $offer->coin_type = 1;
     $offer->shop = new stdClass();
-    $offer->shop->title = 'Кофейня "Венеция"';
-    $offer->shop->location = "ул. Ленина, 4а";
+    $offer->shop->title = 'Coffee House "Kiev"';
+    $offer->shop->location = "Lenina street, 4a";
     return $this->_answerOk($offer);
   }
 
