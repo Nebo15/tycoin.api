7004dd0d9c609cfc4e820df39fad75d4e36b5505
Stanislav Korchagin <korchasa@gmail.com>
2013-01-07 15:03:41 +0400

    cms admin

---
+++ init/migrate/migrations/1357548074_cms_admin.sql
@@ -0,0 +1,10 @@
+INSERT INTO `lmb_cms_user` VALUES(
+  NULL,
+  'support@limb-project.com',
+  'Admin',
+  'b9fb54d1cf88c8c9141cdb01215969221899ff97', /* secret */
+  NULL,
+  'admin',
+  'admin',
+  1
+);
